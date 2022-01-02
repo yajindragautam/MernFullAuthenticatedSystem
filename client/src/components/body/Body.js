ee9df16c4af0e0e03522d8679d63ea22d8162937
Yajindra <yajindragtm@gmail.com>
2022-01-02 15:12:43 +0545

    Have to fix issue

--- client/src/components/body/Body.js
+++ client/src/components/body/Body.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Login from "./auth/Login";
+
 function Body() {
   return (
     <section>
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={Login} exact />
       </Switch>
     </section>
   );
--- client/src/components/body/auth/Login.js
+++ client/src/components/body/auth/Login.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+  err: "",
+  success: "",
+};
 
 function Login() {
-    return (
+  const [user, setUser] = useState(initialState);
+  const { email, password, err, success } = user;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target.value;
+    setUser({ ...user, [name]:value, err: "", success: "" });
+  };
+
+  return (
+    <div className="login_page">
+      <h2>Login</h2>
+      <form>
         <div>
-            Login
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            id="email"
+            value={this.user.email}
+            name="email"
+            onchange={(e) => handleChangeInput(e)}
+          />
         </div>
-    )
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter password"
+            //onchange={handleChangeInput}
+          />
+        </div>
+
+        <div className="row">
+          <button type="submit">Login</button>
+          <Link to="/forget_password">Forget Password</Link>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
